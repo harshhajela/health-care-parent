2a1bb4de0ebd67d26040337468d99e84cec37a8c
Harsh Hajela <60396360+harshhajela@users.noreply.github.com>
2023-12-20 18:04:20 +0000

    Updated script to run docker based services for development

--- script/run-docker-services.sh
+++ script/run-docker-services.sh
@@ -7,3 +7,6 @@ docker run -d \
     mongo:latest
 
 docker run -d -p 1080:1080 -p 1025:1025 --name maildev maildev/maildev
+
+
+docker run --detach --name activemq -p 61616:61616 -p 8161:8161 --rm apache/activemq-artemis:latest-alpine
