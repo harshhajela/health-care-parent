65aeee55d2dbb8e223b53236bd2d8a35bad00b4a
Harsh Hajela <60396360+harshhajela@users.noreply.github.com>
2023-12-25 19:21:00 +0000

    Observability added

--- script/copy-jar-to-s3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#aws s3 cp ../discovery-server/target/*.jar s3://helping-hand-app
-#aws s3 cp ../api-gateway/target/*.jar s3://helping-hand-app
-aws s3 cp ../auth-service/target/*.jar s3://helping-hand-app/JARS/
-#aws s3 cp ../care-provider/target/*.jar s3://helping-hand-app/JARS/
-
--- script/run-docker-services.sh
+++ script/run-docker-services.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-docker run -d \
-    -p 27017:27017 \
-    --name mongo-db \
-    -v data-vol:/data/db \
-    mongo:latest
+##To run MongoDb
+#docker run -d -p 27017:27017 --name mongo-db -v data-vol:/data/db mongo:latest
+#
+##To run maildev
+#docker run -d -p 1080:1080 -p 1025:1025 --name maildev maildev/maildev
+#
+##To run activemq
+#docker run --detach --name activemq -p 61616:61616 -p 8161:8161 --rm apache/activemq-artemis:latest-alpine
 
-docker run -d -p 1080:1080 -p 1025:1025 --name maildev maildev/maildev
+#To run loki
+docker run -d -p 3100:3100 --name loki grafana/loki:main -config.file=/etc/loki/local-config.yaml
 
+#To run prometheus
+docker run -d -p 9090:9090 --name prometheus -v $(pwd)/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml:ro \
+    prom/prometheus:v2.46.0 --enable-feature=exemplar-storage --config.file=/etc/prometheus/prometheus.yml
 
-docker run --detach --name activemq -p 61616:61616 -p 8161:8161 --rm apache/activemq-artemis:latest-alpine
+#To run Tempo
+docker run -d -p 3110:3100 -p 9411:9411 --name tempo \
+    -v $(pwd)/tempo/tempo.yml:/etc/tempo.yaml:ro -v $(pwd)/tempo/tempo-data:/tmp/tempo \
+    grafana/tempo:2.2.2 -config.file=/etc/tempo.yaml
+
+#To run Grafana
+docker run -d -p 3000:3000 --name grafana -v $(pwd)/grafana:/etc/grafana/provisioning/datasources:ro \
+    -e GF_AUTH_ANONYMOUS_ENABLED=true -e GF_AUTH_ANONYMOUS_ORG_ROLE=Admin -e GF_AUTH_DISABLE_LOGIN_FORM=true \
+    grafana/grafana:10.1.0
